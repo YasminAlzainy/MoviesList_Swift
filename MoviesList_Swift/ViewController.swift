f52db5645dfa0f06dd940d98743e5a40ba9ef538
Yasmin Ayman <Yasmin@MacBook-Pro-alkhas-b-Dr.local>
2019-05-13 15:28:56 +0200

    init

---
+++ MoviesList_Swift/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MoviesList_Swift
+//
+//  Created by Yasmin Ayman on 08/09/1440 AH.
+//  Copyright © 1440 ITI.MAD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello");
+    }
+
+
+}
+
---
+++ MoviesList_SwiftTests/MoviesList_SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  MoviesList_SwiftTests.swift
+//  MoviesList_SwiftTests
+//
+//  Created by Yasmin Ayman on 08/09/1440 AH.
+//  Copyright © 1440 ITI.MAD. All rights reserved.
+//
+
+import XCTest
+@testable import MoviesList_Swift
+
+class MoviesList_SwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
